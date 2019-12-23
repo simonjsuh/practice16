cd685eb0cb2f5c0c95d9245dcda1f329414fd4ec
simonjsuh <simon.j.suh@gmail.com>
2019-12-23 15:42:01 -0800

    Updates

---
+++ precache-manifest.c7112944e644d28b4cbf2b7d213ef554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8e45b7aa5ed92885fcc603c0008674",
+    "url": "/practice16/index.html"
+  },
+  {
+    "revision": "c984bc83cd024cb11614",
+    "url": "/practice16/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "766d4194dc348bf713f2",
+    "url": "/practice16/static/js/2.e2bf590d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/practice16/static/js/2.e2bf590d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c984bc83cd024cb11614",
+    "url": "/practice16/static/js/main.5692684d.chunk.js"
+  },
+  {
+    "revision": "0a09a83fbecff04b06f6",
+    "url": "/practice16/static/js/runtime-main.9ca985d8.js"
+  }
+]);
